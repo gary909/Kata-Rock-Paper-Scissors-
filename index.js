1962330fb247974af44f8d4ec542785bf2b5de9d
gary <gary.white@lodgit.com>
2021-12-14 13:03:14 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,6 @@
+const rps = (p1, p2) => {
+};
+
+console.log(rps('rock', 'scissors')) // Return "Player 1 won!"
+console.log(rps('scissors','rock')) // Return "Player 2 won!"
+console.log(rps('paper','paper')) // Return "Draw!"
